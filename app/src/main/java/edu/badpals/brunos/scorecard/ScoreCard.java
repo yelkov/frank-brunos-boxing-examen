c8645947b22f7ca4af9cf166c715c1bc2a90b832
yelkov <yelkovq@gmail.com>
2024-03-14 11:41:51 +0100

    feat(Brunosbox): implementar primera user story

---
+++ app/src/main/java/edu/badpals/brunos/scorecard/ScoreCard.java
@@ -0,0 +1,47 @@
+package edu.badpals.brunos.scorecard;
+
+import edu.badpals.brunos.rounds.RegularRound;
+import edu.badpals.brunos.rounds.Round;
+
+public class ScoreCard {
+
+    private String color;
+    private String redCorner = "";
+    private String blueCorner = "";
+    private String[] judgeScoreCard;
+    private Round[] rounds;
+
+    public ScoreCard(String color){
+        this.color = color;
+    }
+    private String getColor(){
+        return this.color;
+    }
+    public void setRCorner(String boxerName){
+          this.redCorner = boxerName;
+    }
+    public void setBCorner(String boxerName){
+        this.blueCorner = boxerName;
+    }
+    private String getRcorner(){
+        return this.redCorner;
+    }
+    private String getBcorner(){
+        return this.blueCorner;
+    }
+    public byte getNumRounds(){
+        return (byte)rounds.length;
+    }
+
+    @Override
+    public String toString(){
+        return "\t\t\t   "+getColor()+"\t\t\n"
+                +"\t\t"+getBcorner()+"\t"+getRcorner()+"\t\t\n"
+                +"\t\t\t0 rounds\t\t\n" //implementar getNumRounds
+                +"\tRound\tScore\tRound\tScore\tRound\n"
+                +"\tScore\tTotal\t     \tTotal\tScore\n"
+                +"\tRegular round 10 - 9"
+                ;
+    }
+
+}
